0fcf9d3deef9a023a9de78d22a350ec34f3df95e
Cjsah <2684117397@qq.com>
2020-10-01 09:22:49 +0800

    update

--- packages/mcDownload.py
+++ packages/mcDownload.py
@@ -3,45 +3,44 @@ from packages import readConfig
 
 # 读取使用的API
 api = readConfig.readJson("../config.json", "downloadAPI")
-if api != "bmcl":
-    if api == "mcbbs":
-        api_load = "https://download.mcbbs.net/"
-    if api == "mojang":
-        api_load = "http://launchermeta.mojang.com/"
-else:
+
+if api == "bmcl":
     api_load = "https://bmclapi2.bangbang93.com/"
+elif api == "mcbbs":
+    api_load = "https://download.mcbbs.net/"
+elif api == "mojang":
+    api_load = "http://launchermeta.mojang.com/"
 
 
-def getList(What):
-    what = What
+def getList(download):
 
     # Minecraft本体列表
-    if what == "mc":
+    if download == "mc":
         version = requests.get(api_load + "mc/game/version_manifest.json")
 
     # Forge列表
-    if what == "forge":
+    elif download == "forge":
         if api == "mojang":
             version = requests.get("https://bmclapi2.bangbang93.com/forge/download")
         else:
             version = requests.get(api_load + "forge/list/")
 
     # Liteloadder列表
-    if what == "liteloader":
+    elif download == "liteloader":
         if api == "mojang":
             version = requests.get("https://bmclapi2.bangbang93.com/liteloader/list")
         else:
             version = requests.get(api_load + "liteloader/list")
 
     # Optifine列表
-    if what == "optifine":
+    elif download == "optifine":
         if api == "mojang":
             version = requests.get("https://bmclapi2.bangbang93.com/optifine/versionList")
         else:
             version = requests.get(api_load + "optifine/versionList")
 
     # Java列表
-    if what == "java":
+    elif download == "java":
         if api == "mojang":
             version = requests.get("https://bmclapi2.bangbang93.com/java/list")
         else:
